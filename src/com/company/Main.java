0f253d00261a31192bd14e94b649342b3e3445ca
Shivani-Singh28 <shivanisingh2824@gmail.com>
2021-12-14 18:48:32 +0530

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,30 @@
+package com.company;
+
+import java.sql.SQLOutput;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code heress
+        System.out.println("INTEGER");
+        System.out.println("max range:"+Integer.MAX_VALUE);
+        System.out.println(" min range:"+Integer.MIN_VALUE);
+        System.out.println("BYTE");
+        System.out.println(" max range:"+Byte.MAX_VALUE);
+        System.out.println(" min range:"+Byte.MIN_VALUE);
+        System.out.println("DOUBLE");
+        System.out.println(" max range:"+Double.MAX_VALUE);
+        System.out.println( "min range:"+ Double.MIN_VALUE);
+        System.out.println("LONG");
+        System.out.println("max range:"+Long.MAX_VALUE);
+        System.out.println("min range:"+Long.MIN_VALUE);
+        System.out.println("SHORT");
+        System.out.println("max range:"+Short.MAX_VALUE);
+        System.out.println("min range:"+Short.MIN_VALUE);
+
+
+
+
+
+    }
+}
